a4afb4f65edb81fd55ac25ccecd6ce1c3e770e1c
risenb_xinzhongzhi <xinzz_sy_risenb@163.com>
2017-08-31 10:34:29 +0800

    Initial Commit

---
+++ 深拷贝浅拷贝练习/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  深拷贝浅拷贝练习
+//
+//  Created by 辛忠志 on 2017/8/31.
+//  Copyright © 2017年 辛忠志. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
